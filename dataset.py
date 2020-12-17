01562d84d033470635355bc2b7bcd8eb1d8f66d2
오수경 <ohsk23@postech.ac.kr>
2020-12-17 19:54:15 +0900

    Modify dataset from mxnet to torch

--- dataset.py
+++ dataset.py
@@ -124,14 +124,15 @@ class Dataset(object):
         print("+------------------------------+")
 
 
-dataset = Dataset(DATA_DIR, TRAIN_TXT, TEST_TXT)
-dataset.print_stats()
+# def test_dataset():
+#     dataset = Dataset(DATA_DIR, TRAIN_TXT, TEST_TXT)
+#     dataset.print_stats()
 
-test_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+#     test_transform = transforms.Compose([
+#         transforms.Resize((224, 224)),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ])
 
-img_dataset = ImageData(dataset.test, test_transform)
-print(img_dataset[0])
+#     img_dataset = ImageData(dataset.test, test_transform)
+#     print(img_dataset[0])
