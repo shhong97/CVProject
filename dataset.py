456e1ca50ae0f9e4dfeb3ea0f68066270986acc0
오수경 <ohsk23@postech.ac.kr>
2020-12-19 00:31:19 +0900

    Fix ImageData bbox typeerror

--- dataset.py
+++ dataset.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from PIL import Image
+from scipy import io
 
 from torchvision import transforms
 import torchvision.transforms.functional as tf
@@ -30,6 +31,7 @@ import os
 DATA_DIR = './CUB_200_2011'
 TRAIN_TXT = './meta/CUB200/train.txt'
 TEST_TXT = './meta/CUB200/test.txt'
+BBOX_TXT = './meta/CUB200/bbox.txt'
 
 
 class ImageData(torch.utils.data.Dataset):
@@ -43,13 +45,14 @@ class ImageData(torch.utils.data.Dataset):
 
         # image = tf.to_tensor(image)
 
-        if bbox is not None:
-            x, y, w, h = bbox
-            image = image[y:min(y+h, image.shape[0]),
-                          x:min(x+w, image.shape[1])]
-
         if self.transform is not None:
             image = self.transform(image)
+
+        if bbox is not None:
+            x, y, w, h = bbox
+            _, width, height = image.size()
+            image = image[y:min(y+h, height),
+                          x:min(x+w, width)]
 
         return image, image_id
 
@@ -125,8 +128,108 @@ class Dataset(object):
         print("+------------------------------+")
 
 
+CARS_DIR = './CARS_196'
+CARS_TRAIN_MAT = './CARS_196/devkit/cars_train_annos.mat'
+CARS_TEST_MAT = './CARS_196/devkit/cars_test_annos.mat'
+
+
+class CARS_Dataset():
+    def __init__(self, dataset_dir, train_mat, test_mat):
+        self.dataset_dir = dataset_dir
+        self._bbox_train = None
+        self._bbox_test = None
+
+        train, num_train_ids = self._load_train(train_mat)
+        test, num_test_ids = self._load_test(test_mat)
+
+        self.train = train
+        self.test = test
+
+        self.num_train_ids = num_train_ids
+        self.num_test_ids = num_test_ids
+
+    # TODO: Refactoring with non duplicated codes
+    def _load_train(self, meta_file):
+        datasets = []
+        num_class_ids = 0
+
+        cars_annos_train = io.loadmat(meta_file)
+        self._bbox_train = {}
+
+        for d in cars_annos_train['annotations'][0]:
+            num_class_ids += 1
+            bbox_x1 = d[0][0][0]
+            bbox_y1 = d[1][0][0]
+            bbox_x2 = d[2][0][0]
+            bbox_y2 = d[3][0][0]
+            im_path = d[4][0]
+
+            x = min(bbox_x1, bbox_x2)
+            y = min(bbox_y1, bbox_y2)
+            w = max(bbox_x1, bbox_x2) - x
+            h = max(bbox_y1, bbox_y2) - y
+
+            image_id = int(im_path[:5])
+            self._bbox_train[int(image_id)] = [int(x), int(y), int(w), int(h)]
+            # image_label = str(image_id) + '_train'
+            bbox = self._bbox_train[int(image_id)]
+            datasets.append(
+                (os.path.join(self.dataset_dir, 'cars_train/' + str(im_path)), image_id, bbox))
+
+        return datasets, num_class_ids
+
+    def _load_test(self, meta_file):
+        datasets = []
+        num_class_ids = 0
+
+        cars_annos_test = io.loadmat(meta_file)
+        self._bbox_test = {}
+
+        for d in cars_annos_test['annotations'][0]:
+            num_class_ids += 1
+            bbox_x1 = d[0][0][0]
+            bbox_y1 = d[1][0][0]
+            bbox_x2 = d[2][0][0]
+            bbox_y2 = d[3][0][0]
+            im_path = d[4][0]
+
+            x = min(bbox_x1, bbox_x2)
+            y = min(bbox_y1, bbox_y2)
+            w = max(bbox_x1, bbox_x2) - x
+            h = max(bbox_y1, bbox_y2) - y
+
+            image_id = int(im_path[:5])
+            self._bbox_test[int(image_id)] = [int(x), int(y), int(w), int(h)]
+            # image_label = str(image_id) + '_test'
+            bbox = self._bbox_test[int(image_id)]
+            datasets.append(
+                (os.path.join(self.dataset_dir, 'cars_test/' + str(im_path)), image_id, bbox))
+
+        return datasets, num_class_ids
+
+    def print_stats(self):
+        num_total_ids = self.num_train_ids + self.num_test_ids
+
+        num_train_images = len(self.train)
+        num_test_images = len(self.test)
+        num_total_images = num_train_images + num_test_images
+
+        print("###### Dataset Statistics ######")
+        print("+------------------------------+")
+        print("| Subset  | #Classes | #Images |")
+        print("+------------------------------+")
+        print("| Train   |    {:5d} | {:7d} |".format(
+            self.num_train_ids, num_train_images))
+        print("| Test    |    {:5d} | {:7d} |".format(
+            self.num_test_ids, num_test_images))
+        print("+------------------------------+")
+        print("| Total   |    {:5d} | {:7d} |".format(
+            num_total_ids, num_total_images))
+        print("+------------------------------+")
+
+
 def test_dataset():
-    dataset = Dataset(DATA_DIR, TRAIN_TXT, TEST_TXT)
+    dataset = Dataset(DATA_DIR, TRAIN_TXT, TEST_TXT, BBOX_TXT)
     dataset.print_stats()
 
     test_transform = transforms.Compose([
@@ -138,3 +241,21 @@ def test_dataset():
     img_dataset = ImageData(dataset.test, test_transform)
     for x in img_dataset:
         print(x[0].shape)
+
+
+def test_cars_dataset():
+    dataset = CARS_Dataset(CARS_DIR, CARS_TRAIN_MAT, CARS_TEST_MAT)
+    dataset.print_stats()
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    img_dataset = ImageData(dataset.test, test_transform)
+    for x in img_dataset:
+
+        print(x[0].shape)
+
+
+# test_dataset()
